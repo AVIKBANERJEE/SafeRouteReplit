7ba0d0ccca90aaeb06a92abac1ac377f0028195b
Samannway Sil <samannway1@gmail.com>
2023-11-25 20:03:36 +0530

    minorchanges

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.secret_key='bullshitsomething'
 def create_app():
     # Configuration options can be set here
     #app.config['SECRET_KEY'] = 'your_secret_key'
